5a6e3c30269f7efd9bfbfa2b1cece8bb80f3e341
Hipolito-Tapia <u20171a057@upc.edu.pe>
2021-12-05 02:19:47 -0500

    segundo envio

--- src/librodeitel/PruebaAccesoMiembros.java
+++ src/librodeitel/PruebaAccesoMiembros.java
@@ -4,6 +4,12 @@ public class PruebaAccesoMiembros {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Tiempo1 tiempo = new Tiempo1(); //crea e inicializa el objeto tiempo
+		
+		/*tiempo.hora =7;
+		tiempo.minuto=15;
+		tiempo.segundo=30*/
+		
 
 	}
 
