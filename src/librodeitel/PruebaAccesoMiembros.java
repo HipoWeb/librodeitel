82b64c426908d84d871235d7df8cf2240d63f855
Hipolito-Tapia <u20171a057@upc.edu.pe>
2021-12-05 01:32:32 -0500

    envio desde la pc

---
+++ src/librodeitel/PruebaAccesoMiembros.java
@@ -0,0 +1,10 @@
+package librodeitel;
+
+public class PruebaAccesoMiembros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/librodeitel/PruebaTiempo1.java
+++ src/librodeitel/PruebaTiempo1.java
@@ -6,6 +6,7 @@ public class PruebaTiempo1 {
 		// TODO Auto-generated method stub
 		Tiempo1 tiempo = new Tiempo1(); // invoca el constructor de Tiempo1
 		// imprime representaciones de cadena del tiempo
+		
 		mostrarTiempo("Despues de crear el objeto tiempo", tiempo);
 		System.out.println();
 		
@@ -13,7 +14,18 @@ public class PruebaTiempo1 {
 		tiempo.establecerTiempo(13, 27, 6);
 		mostrarTiempo("Despues de llamar a establecerTiempo", tiempo);
 		System.out.println();
+		
+		try {
+			tiempo.establecerTiempo(99, 99, 99);
+		} catch (IllegalArgumentException e) {
+			// TODO: handle exception
+			System.out.printf("Excepcion: %s%n%n",e.getMessage());
+		}
+		mostrarTiempo("despues de llamar a establecer tiempo con valores invalidor", tiempo);
 	}
-
+	private static void mostrarTiempo(String encabezado, Tiempo1 t) {
+		System.out.printf("%s%nTiempo Universal: %s%n tiempo estandar: %s%n ",encabezado, t.aStringUniversal(),t.tosString());
+		
+	}
 
 }
